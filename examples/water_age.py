0c90e8072f2ab48d40f0a6ed8b1bd8f9ab8e4a34
André Artelt <aartelt@techfak.de>
2024-02-25 10:22:40 +0100

    Add example on water age analysis

---
+++ examples/water_age.py
@@ -0,0 +1,22 @@
+from epyt_flow.data.networks import load_richmond
+from epyt_flow.simulation import WaterDistributionNetworkScenarioSimulator
+
+
+if __name__ == "__main__":
+    # Open/Create a new scenario based on the Richmond network
+    network_config = load_richmond()
+    with WaterDistributionNetworkScenarioSimulator(scenario_config=network_config) as sim:
+        # Set simulation duration to two days
+        sim.set_general_parameters(simulation_duration=2)
+
+        # Enable water age analysis
+        sim.enable_waterage_analysis()
+
+        # Places quality sensors at all nodes -- i.e. measuring the water age at each node
+        sim.set_node_quality_sensors(sensor_locations=sim.sensor_config.nodes)
+
+        # Run simulation
+        scada_data = sim.run_simulation()
+
+        # Retrieve and show simulated water age at all nodes
+        print(f"Water age: {scada_data.get_data_nodes_quality()}")
