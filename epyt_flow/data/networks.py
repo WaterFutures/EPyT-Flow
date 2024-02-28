fc6f1f72fdc9768abfd43e15ed066bbf8c3211f8
André Artelt <aartelt@techfak.de>
2024-02-28 08:54:55 +0100

    Add more networks

--- epyt_flow/data/networks.py
+++ epyt_flow/data/networks.py
@@ -147,6 +147,30 @@ def load_net3(download_dir: str = get_temp_folder()) -> ScenarioConfig:
     return load_inp(f_in)
 
 
+def load_net6(download_dir: str = get_temp_folder()) -> ScenarioConfig:
+    """
+    Loads (and downloads if necessary) the Net6 network.
+
+    Parameters
+    ----------
+    download_dir : `str`, optional
+        Path to the directory where the .inp file is stored.
+
+        The default is the OS specific temporary directory (e.g. "C:\\\\temp", "/tmp/", etc.)
+
+    Returns
+    -------
+    :class:`~epyt_flow.simulation.scenario_config.ScenarioConfig`
+        Net6 network loaded into a scenario configuration that can be passed on to
+        :class:`~epyt_flow.simulation.scenario_simulator.WaterDistributionNetworkScenarioSimulator`.
+    """
+    f_in = os.path.join(download_dir, "Net6.inp")
+    url = "https://github.com/OpenWaterAnalytics/WNTR/raw/main/examples/networks/Net6.inp"
+
+    download_if_necessary(f_in, url)
+    return load_inp(f_in)
+
+
 def load_richmond(download_dir: str = get_temp_folder()) -> ScenarioConfig:
     """
     Loads (and downloads if necessary) the Richmond network.
@@ -172,6 +196,131 @@ def load_richmond(download_dir: str = get_temp_folder()) -> ScenarioConfig:
     return load_inp(f_in)
 
 
+def load_micropolis(download_dir: str = get_temp_folder()) -> ScenarioConfig:
+    """
+    Loads (and downloads if necessary) the MICROPOLIS network.
+
+    Parameters
+    ----------
+    download_dir : `str`, optional
+        Path to the directory where the .inp file is stored.
+
+        The default is the OS specific temporary directory (e.g. "C:\\\\temp", "/tmp/", etc.)
+
+    Returns
+    -------
+    :class:`~epyt_flow.simulation.scenario_config.ScenarioConfig`
+        MICROPOLIS network loaded into a scenario configuration that can be passed on to
+        :class:`~epyt_flow.simulation.scenario_simulator.WaterDistributionNetworkScenarioSimulator`.
+    """
+    f_in = os.path.join(download_dir, "MICROPOLIS_v1.inp")
+    url = "https://github.com/OpenWaterAnalytics/EPyT/raw/main/epyt/networks/asce-tf-wdst/" +\
+        "MICROPOLIS_v1.inp"
+
+    download_if_necessary(f_in, url)
+    return load_inp(f_in)
+
+
+def load_balerma(download_dir: str = get_temp_folder()) -> ScenarioConfig:
+    """
+    Loads (and downloads if necessary) the Balerma network.
+
+    Parameters
+    ----------
+    download_dir : `str`, optional
+        Path to the directory where the .inp file is stored.
+
+        The default is the OS specific temporary directory (e.g. "C:\\\\temp", "/tmp/", etc.)
+
+    Returns
+    -------
+    :class:`~epyt_flow.simulation.scenario_config.ScenarioConfig`
+        Balerma network loaded into a scenario configuration that can be passed on to
+        :class:`~epyt_flow.simulation.scenario_simulator.WaterDistributionNetworkScenarioSimulator`.
+    """
+    f_in = os.path.join(download_dir, "Balerma.inp")
+    url = "https://github.com/OpenWaterAnalytics/EPyT/raw/main/epyt/networks/" +\
+        "asce-tf-wdst/Balerma.inp"
+
+    download_if_necessary(f_in, url)
+    return load_inp(f_in)
+
+
+def load_rural(download_dir: str = get_temp_folder()) -> ScenarioConfig:
+    """
+    Loads (and downloads if necessary) the Rural network.
+
+    Parameters
+    ----------
+    download_dir : `str`, optional
+        Path to the directory where the .inp file is stored.
+
+        The default is the OS specific temporary directory (e.g. "C:\\\\temp", "/tmp/", etc.)
+
+    Returns
+    -------
+    :class:`~epyt_flow.simulation.scenario_config.ScenarioConfig`
+        Rural network loaded into a scenario configuration that can be passed on to
+        :class:`~epyt_flow.simulation.scenario_simulator.WaterDistributionNetworkScenarioSimulator`.
+    """
+    f_in = os.path.join(download_dir, "RuralNetwork.inp")
+    url = "https://github.com/OpenWaterAnalytics/EPyT/raw/main/epyt/networks/" +\
+        "asce-tf-wdst/RuralNetwork.inp"
+
+    download_if_necessary(f_in, url)
+    return load_inp(f_in)
+
+
+def load_bwsn1(download_dir: str = get_temp_folder()) -> ScenarioConfig:
+    """
+    Loads (and downloads if necessary) the BWSN-1 network.
+
+    Parameters
+    ----------
+    download_dir : `str`, optional
+        Path to the directory where the .inp file is stored.
+
+        The default is the OS specific temporary directory (e.g. "C:\\\\temp", "/tmp/", etc.)
+
+    Returns
+    -------
+    :class:`~epyt_flow.simulation.scenario_config.ScenarioConfig`
+        BWSN-1 network loaded into a scenario configuration that can be passed on to
+        :class:`~epyt_flow.simulation.scenario_simulator.WaterDistributionNetworkScenarioSimulator`.
+    """
+    f_in = os.path.join(download_dir, "BWSN_Network_1.inp")
+    url = "https://github.com/OpenWaterAnalytics/EPyT/raw/main/epyt/networks/" +\
+        "asce-tf-wdst/BWSN_Network_1.inp"
+
+    download_if_necessary(f_in, url)
+    return load_inp(f_in)
+
+
+def load_bwsn2(download_dir: str = get_temp_folder()) -> ScenarioConfig:
+    """
+    Loads (and downloads if necessary) the BWSN-2 network.
+
+    Parameters
+    ----------
+    download_dir : `str`, optional
+        Path to the directory where the .inp file is stored.
+
+        The default is the OS specific temporary directory (e.g. "C:\\\\temp", "/tmp/", etc.)
+
+    Returns
+    -------
+    :class:`~epyt_flow.simulation.scenario_config.ScenarioConfig`
+        BWSN-2 network loaded into a scenario configuration that can be passed on to
+        :class:`~epyt_flow.simulation.scenario_simulator.WaterDistributionNetworkScenarioSimulator`.
+    """
+    f_in = os.path.join(download_dir, "BWSN_Network_2.inp")
+    url = "https://github.com/OpenWaterAnalytics/EPyT/raw/main/epyt/networks/" +\
+        "asce-tf-wdst/BWSN_Network_2.inp"
+
+    download_if_necessary(f_in, url)
+    return load_inp(f_in)
+
+
 def load_anytown(download_dir: str = get_temp_folder()) -> ScenarioConfig:
     """
     Loads (and downloads if necessary) the Anytown network.
@@ -192,6 +341,54 @@ def load_anytown(download_dir: str = get_temp_folder()) -> ScenarioConfig:
     f_in = os.path.join(download_dir, "Anytown.inp")
     url = "https://raw.githubusercontent.com/OpenWaterAnalytics/EPyT/main/epyt/networks/" +\
         "asce-tf-wdst/Anytown.inp"
+
+    download_if_necessary(f_in, url)
+    return load_inp(f_in)
+
+
+def load_dtown(download_dir: str = get_temp_folder()) -> ScenarioConfig:
+    """
+    Loads (and downloads if necessary) the D-Town network.
+
+    Parameters
+    ----------
+    download_dir : `str`, optional
+        Path to the directory where the .inp file is stored.
+
+        The default is the OS specific temporary directory (e.g. "C:\\\\temp", "/tmp/", etc.)
+
+    Returns
+    -------
+    :class:`~epyt_flow.simulation.scenario_config.ScenarioConfig`
+        D-Town network loaded into a scenario configuration that can be passed on to
+        :class:`~epyt_flow.simulation.scenario_simulator.WaterDistributionNetworkScenarioSimulator`.
+    """
+    f_in = os.path.join(download_dir, "d-town.inp")
+    url = "https://www.exeter.ac.uk/media/universityofexeter/emps/research/cws/downloads/d-town.inp"
+
+    download_if_necessary(f_in, url)
+    return load_inp(f_in)
+
+
+def load_ctown(download_dir: str = get_temp_folder()) -> ScenarioConfig:
+    """
+    Loads (and downloads if necessary) the C-Town network.
+
+    Parameters
+    ----------
+    download_dir : `str`, optional
+        Path to the directory where the .inp file is stored.
+
+        The default is the OS specific temporary directory (e.g. "C:\\\\temp", "/tmp/", etc.)
+
+    Returns
+    -------
+    :class:`~epyt_flow.simulation.scenario_config.ScenarioConfig`
+        C-Town network loaded into a scenario configuration that can be passed on to
+        :class:`~epyt_flow.simulation.scenario_simulator.WaterDistributionNetworkScenarioSimulator`.
+    """
+    f_in = os.path.join(download_dir, "CTOWN.INP")
+    url = "https://github.com/scy-phy/www.batadal.net/raw/master/data/CTOWN.INP"
 
     download_if_necessary(f_in, url)
     return load_inp(f_in)
@@ -270,10 +467,10 @@ def load_hanoi(download_dir: str = get_temp_folder(),
     return config
 
 
-def load_ltown(download_dir: str = get_temp_folder(),
+def load_ltown(download_dir: str = get_temp_folder(), use_realistic_demands: bool = False,
                include_default_sensor_placement: bool = False) -> ScenarioConfig:
     """
-    Loads (and downloads if necessary) the L-TOWN network.
+    Loads (and downloads if necessary) the L-TOWN_v2 network.
 
     Parameters
     ----------
@@ -281,21 +478,80 @@ def load_ltown(download_dir: str = get_temp_folder(),
         Path to the directory where the .inp file is stored.
 
         The default is the OS specific temporary directory (e.g. "C:\\\\temp", "/tmp/", etc.)
+    use_realistic_demands : `bool`, optional
+        If True, realistic demands from the BattLeDIM challenge will be included,
+        toy demands will be included otherwise.
+
+        The default is False
     include_default_sensor_placement : `bool`, optional
-        If True, a default sensor placement (as proposed in the BattLeDim challenge --
-        see https://github.com/KIOS-Research/BattLeDIM) will be included in the returned
-        scenario configuration.
+        If True, the L-TOWN default sensor placement will be included
+        in the returned scenario configuration.
 
         The default is False
 
     Returns
     -------
     :class:`~epyt_flow.simulation.scenario_config.ScenarioConfig`
-        L-TOWN network loaded into a scenario configuration that can be passed on to
+        L-TOWN_v2 network loaded into a scenario configuration that can be passed on to
         :class:`~epyt_flow.simulation.scenario_simulator.WaterDistributionNetworkScenarioSimulator`.
     """
-    f_in = os.path.join(download_dir, "L-TOWN.inp")
-    url = "https://raw.githubusercontent.com/OpenWaterAnalytics/EPyT/main/epyt/networks/L-TOWN.inp"
+    f_inp = "L-TOWN_v2_Model.inp" if use_realistic_demands is False else "L-TOWN_v2_Real.inp"
+
+    f_in = os.path.join(download_dir, f_inp)
+    if use_realistic_demands:
+        url = "https://zenodo.org/records/4017659/files/L-TOWN.inp?download=1"
+    else:
+        url = "https://zenodo.org/records/4017659/files/L-TOWN_Real.inp?download=1"
+
+    download_if_necessary(f_in, url)
+    config = load_inp(f_in)
+
+    if include_default_sensor_placement is True:
+        sensor_config = config.sensor_config
+        sensor_config.pressure_sensors = ["n54", "n105", "n114", "n163", "n188", "n229", "n288",
+                                          "n296", "n332", "n342", "n410", "n415", "n429", "n458",
+                                          "n469", "n495", "n506", "n516", "n519", "n549", "n613",
+                                          "n636", "n644", "n679", "n722", "n726", "n740", "n752",
+                                          "n769"]
+        sensor_config.flow_sensors = ["p227", "p235"]
+
+        config = ScenarioConfig(f_inp_in=config.f_inp_in, sensor_config=sensor_config)
+
+    return config
+
+
+def load_ltown_a(download_dir: str = get_temp_folder(), use_realistic_demands: bool = False,
+                 include_default_sensor_placement: bool = False) -> ScenarioConfig:
+    """
+    Loads (and downloads if necessary) the L-TOWN-A network (area "A" of the L-TOWN network).
+
+    Parameters
+    ----------
+    download_dir : `str`, optional
+        Path to the directory where the .inp file is stored.
+
+        The default is the OS specific temporary directory (e.g. "C:\\\\temp", "/tmp/", etc.)
+    use_realistic_demands : `bool`, optional
+        If True, realistic demands from the BattLeDIM challenge will be included,
+        toy demands will be included otherwise.
+
+        The default is False
+    include_default_sensor_placement : `bool`, optional
+        If True, the L-TOWN default sensor placement will be included
+        in the returned scenario configuration.
+
+        The default is False
+
+    Returns
+    -------
+    :class:`~epyt_flow.simulation.scenario_config.ScenarioConfig`
+        L-TOWN-A network loaded into a scenario configuration that can be passed on to
+        :class:`~epyt_flow.simulation.scenario_simulator.WaterDistributionNetworkScenarioSimulator`.
+    """
+    f_inp = "L-TOWN_v2-A_Model.inp" if use_realistic_demands is False else "L-TOWN_v2-A_Real.inp"
+
+    f_in = os.path.join(download_dir, f_inp)
+    url = f"https://filedn.com/lumBFq2P9S74PNoLPWtzxG4/EPyT-Flow/Networks/{f_inp}"
 
     download_if_necessary(f_in, url)
     config = load_inp(f_in)
