7991b0dc74a552306362678ec435c3090d00d4f1
André Artelt <aartelt@techfak.de>
2024-04-01 19:15:01 +0200

    Add example of chlorine injection

---
+++ examples/chlorine_injection.py
@@ -0,0 +1,36 @@
+"""
+Example of adding a chlorine injection to a scenario.
+"""
+import numpy
+from epyt_flow.data.networks import load_hanoi
+from epyt_flow.simulation import ScenarioSimulator, ToolkitConstants
+from epyt_flow.utils import to_seconds
+
+
+if __name__ == "__main__":
+    # Load Hanoi network
+    network_config = load_hanoi()
+
+    # Create scenario
+    with ScenarioSimulator(scenario_config=network_config) as sim:
+        # Set simulation duration to two days hours
+        sim.set_general_parameters(simulation_duration=to_seconds(days=2))
+
+        # Enable chemical analysis
+        sim.enable_chemical_analysis()
+
+        # Sets the concentration at node "1" (reservoir) to 1.0 for all time steps.
+        sim.add_quality_source(node_id="1",
+                               pattern=numpy.array([1.]),
+                               source_type=ToolkitConstants.EN_CONCEN)
+
+        # Places quality sensors at all nodes --
+        # i.e. measuring the chemical concentration at all nodes
+        sim.set_node_quality_sensors(sensor_locations=sim.sensor_config.nodes)
+
+        # Run simulation
+        scada_data = sim.run_simulation()
+
+        # Retrieve simulated chemical concentrations at all nodes
+        nodes_quality = scada_data.get_data_nodes_quality()
+        print(nodes_quality)
