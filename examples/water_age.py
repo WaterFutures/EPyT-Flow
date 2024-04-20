e3e196d0beaad81e1cdde8eca1e5e01a6100f36e
André Artelt <aartelt@techfak.de>
2024-04-20 09:20:27 +0200

    Update water age example

--- examples/water_age.py
+++ examples/water_age.py
@@ -1,14 +1,14 @@
 """
 Example on water age analysis.
 """
-from epyt_flow.data.networks import load_richmond
+from epyt_flow.data.networks import load_ctown
 from epyt_flow.simulation import ScenarioSimulator
 from epyt_flow.utils import to_seconds
 
 
 if __name__ == "__main__":
-    # Create a new scenario simulation based on the Richmond network
-    network_config = load_richmond()
+    # Create a new scenario simulation based on the C-Town network
+    network_config = load_ctown()
     with ScenarioSimulator(scenario_config=network_config) as sim:
         # Set simulation duration to two days
         sim.set_general_parameters(simulation_duration=to_seconds(days=2))
