f88375c391d8019e57b2a4fa0d07030488877cd6
André Artelt <aartelt@techfak.de>
2024-05-31 14:13:42 +0200

    Refactor code for compiling EPANET

---
+++ epyt_flow/EPANET/compile_linux.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mkdir -p "../customlibs/"
+gcc -w -O3 -march=native -shared -fPIC -o "../customlibs/libepanet2_2.so" EPANET/SRC_engines/*.c -IEPANET/SRC_engines/include -lc -lm -pthread
+gcc -w -O3 -march=native -fPIC -shared -o "../customlibs/libepanetmsx2_2_0.so" -fopenmp -Depanetmsx_EXPORTS -IEPANET-MSX/Src/include -IEPANET/SRC_engines/include EPANET-MSX/Src/*.c -Wl,-rpath=. "../customlibs/libepanet2_2.so" -lm -lgomp -lpthread
--- epyt_flow/__init__.py
+++ epyt_flow/__init__.py
@@ -7,24 +7,31 @@ import os
 with open(os.path.join(os.path.dirname(__file__), 'VERSION'), encoding="utf-8") as f:
     VERSION = f.read().strip()
 
+
+def compile_libraries_unix(lib_epanet_name, compile_script_name):
+    """Compile EPANET and EPANET-MSX libraries if needed."""
+    path_to_custom_libs = os.path.join(os.path.dirname(__file__), "customlibs")
+    path_to_lib_epanet = os.path.join(path_to_custom_libs, lib_epanet_name)
+    path_to_epanet = os.path.join(os.path.dirname(__file__), "EPANET")
+
+    update = False
+    if os.path.isfile(path_to_lib_epanet):
+        if os.path.getmtime(__file__) > os.path.getmtime(path_to_lib_epanet):
+            update = True
+
+    if not os.path.isfile(path_to_lib_epanet) or update:
+        if shutil.which("gcc") is not None:
+            print("Compiling EPANET and EPANET-MSX...")
+            try:
+                subprocess.check_call(f"cd \"{path_to_epanet}\"; bash {compile_script_name}",
+                                      shell=True)
+                print("Done")
+            except subprocess.CalledProcessError as ex:
+                print(f"Compilation failed\n{ex}")
+        else:
+            warnings.warn("GCC is not available to compile the required libraries.\n" +
+                          "Falling back to pre-compiled library shipped by EPyT.")
+
+
 if sys.platform.startswith("linux"):
-    def compile_libraries():
-        """Compile EPANET and EPANET-MSX libraries if needed."""
-        path_to_custom_libs = os.path.join(os.path.dirname(__file__), "customlibs")
-        path_to_lib_epanet = os.path.join(path_to_custom_libs, "libepanet2_2.so")
-        path_to_epanet = os.path.join(os.path.dirname(__file__), "EPANET")
-
-        update = False
-        if os.path.isfile(path_to_lib_epanet):
-            if os.path.getmtime(__file__) > os.path.getmtime(path_to_lib_epanet):
-                update = True
-
-        if not os.path.isfile(path_to_lib_epanet) or update:
-            if shutil.which("gcc") is not None:
-                print("Compiling EPANET and EPANET-MSX...")
-                subprocess.check_call(f"cd \"{path_to_epanet}\"; bash compile.sh", shell=True)
-            else:
-                warnings.warn("GCC is not available to compile the required libraries.\n" +
-                              "Falling back to pre-compiled library shipped by EPyT.")
-
-    compile_libraries()
+    compile_libraries_unix("libepanet2_2.so", "compile_linux.sh")
