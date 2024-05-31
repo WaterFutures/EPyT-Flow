25434ba3c249f80640d1985437bc22b4eecde964
André Artelt <aartelt@techfak.de>
2024-05-31 09:08:02 +0200

    Update uncertainty example

--- examples/uncertainties.py
+++ examples/uncertainties.py
@@ -1,6 +1,7 @@
 """
 Example of adding uncertainty with respect to WDN parameters (e.g. demand pattern).
 """
+import numpy as np
 from epyt_flow.data.networks import load_ltown
 from epyt_flow.simulation import ScenarioSimulator, ModelUncertainty, RelativeUniformUncertainty
 from epyt_flow.utils import to_seconds
@@ -12,14 +13,20 @@ if __name__ == "__main__":
 
     # Create scenario
     with ScenarioSimulator(scenario_config=network_config) as sim:
-        # Set simulation duration to five hours
-        sim.set_general_parameters(simulation_duration=to_seconds(hours=5))
+        # Set simulation duration to three hours
+        sim.set_general_parameters(simulation_duration=to_seconds(hours=3))
 
         # Add uncertainty (i.e. randomness) with respect to the demand pattern --
         # i.e. demand pattern values can deviate up to 25% from their original value.
+        # Consequently, the simulation is no longer deterministic and the results vary
+        # from run to run.
         uc = RelativeUniformUncertainty(low=0.75, high=1.25)
         sim.set_model_uncertainty(ModelUncertainty(demand_pattern_uncertainty=uc))
 
-        # Run simulation and retrieve sensor readings at node "n105"
-        scada_data = sim.run_simulation()
-        print(scada_data.get_data_pressures(sensor_locations=["n105"]))
+        # Run simulation three times and retrieve sensor readings at node "n105"
+        measurements = []
+        for _ in range(3):
+            scada_data = sim.run_simulation()
+            measurements.append(scada_data.get_data_pressures(sensor_locations=["n105"]).
+                                flatten().tolist())
+        print(np.mean(measurements, axis=0), np.var(measurements, axis=0))
