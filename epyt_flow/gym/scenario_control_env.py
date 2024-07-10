0b2e5423994ed09f6154bb10cb5b924cc9c333f7
André Artelt <aartelt@techfak.de>
2024-07-10 07:57:58 +0200

    Add helper functions to ScenarioControlEnv

--- epyt_flow/gym/scenario_control_env.py
+++ epyt_flow/gym/scenario_control_env.py
@@ -3,6 +3,8 @@ Module provides a base class for control environments.
 """
 from abc import abstractmethod, ABC
 from typing import Union
+import warnings
+import numpy as np
 
 from ..simulation import ScenarioSimulator, ScenarioConfig, ScadaData
 
@@ -93,8 +95,91 @@ class ScenarioControlEnv(ABC):
             else:
                 return None, True
 
+    def set_pump_status(self, pump_id: str, status: int) -> None:
+        """
+        Sets the status of a pump.
+
+        Parameters
+        ----------
+        pump_id : `str`
+            ID of the pump for which the status is set.
+        status : `int`
+            New status of the pump -- either active (i.e. open) or inactive (i.e. closed).
+
+            Must be one of the following constants defined in
+            :class:`~epyt_flow.simulation.events.actuator_events.ActuatorConstants`:
+
+                - EN_CLOSED  = 0
+                - EN_OPEN    = 1
+        """
+        pump_idx = self._scenario_sim.epanet_api.getLinkPumpNameID().index(pump_id)
+        pump_link_idx = self._scenario_sim.epanet_api.getLinkPumpIndex(pump_idx + 1)
+        self._scenario_sim.epanet_api.setLinkStatus(pump_link_idx, status)
+
+    def set_pump_speed(self, pump_id: str, speed: float) -> None:
+        """
+        Sets the speed of a pump.
+
+        Parameters
+        ----------
+        pump_id : `str`
+            ID of the pump for which the pump speed is set.
+        speed : `float`
+            New pump speed.
+        """
+        pump_idx = self._scenario_sim.epanet_api.getLinkPumpNameID().index(pump_id)
+        pattern_idx = self._scenario_sim.epanet_api.getLinkPumpPatternIndex(pump_idx + 1)
+
+        if pattern_idx == 0:
+            warnings.warn(f"No pattern for pump '{pump_id}' found -- a new pattern is created")
+            pattern_idx = self._scenario_sim.epanet_api.addPattern(f"pump_speed_{pump_id}")
+            self._scenario_sim.epanet_api.setLinkPumpPatternIndex(pattern_idx)
+
+        self._scenario_sim.epanet_api.setPattern(pattern_idx, np.array([speed]))
+
+    def set_valve_status(self, valve_id: str, status: int) -> None:
+        """
+        Sets the status of a valve.
+
+        Parameters
+        ----------
+        valve_id : `str`
+            ID of the valve for which the status is set.
+        status : `int`
+            New status of the valve -- either open or closed.
+
+            Must be one of the following constants defined in
+            :class:`~epyt_flow.simulation.events.actuator_events.ActuatorConstants`:
+
+                - EN_CLOSED  = 0
+                - EN_OPEN    = 1
+        """
+        valve_idx = self._scenario_sim.epanet_api.getLinkValveNameID().index(valve_id)
+        valve_link_idx = self._scenario_sim.epanet_api.getLinkValveIndex(valve_idx + 1)
+        self._scenario_sim.epanet_api.setLinkStatus(valve_link_idx, status)
+
+    def set_node_quality_source_value(self, node_id: str, pattern_id: str,
+                                      qual_value: float) -> None:
+        """
+        Sets the quality source at a particular node to a specific value -- e.g.
+        setting the chlorine concentration injection to a specified value.
+
+        Parameters
+        ----------
+        node_id : `str`
+            ID of the node.
+        pattern_id : `str`
+            ID of the quality pattern at the specific node.
+        qual_value : `float`
+            New quality source value.
+        """
+        node_idx = self._scenario_sim.epanet_api.getNodeIndex(node_id)
+        pattern_idx = self._scenario_sim.epanet_api.getPatternIndex(pattern_id)
+        self._scenario_sim.epanet_api.setNodeSourceQuality(node_idx, 1)
+        self._scenario_sim.epanet_api.setPattern(pattern_idx, np.array([qual_value]))
+
     @abstractmethod
-    def step(self) -> Union[tuple[ScadaData, float, bool], tuple[ScadaData, float]]:
+    def step(self, *actions) -> Union[tuple[ScadaData, float, bool], tuple[ScadaData, float]]:
         """
         Performs the next step by applying an action and observing
         the consequences (SCADA data, reward, terminated).
