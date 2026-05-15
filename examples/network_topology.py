d2fdbb40246f11957d06eba46731afa44ac5e5ff
André Artelt <aartelt@techfak.de>
2026-05-15 10:41:29 +0200

    Update network topology examples

--- examples/network_topology.py
+++ examples/network_topology.py
@@ -2,8 +2,8 @@
 Example of retrieving the network topology.
 """
 from epyt_flow.data.networks import load_net1
-from epyt_flow.simulation import ScenarioSimulator
-from epyt_flow.topology import flowunit_to_str, UNITS_SIMETRIC, UNITS_USCUSTOM
+from epyt_flow.simulation import ScenarioSimulator, EpanetConstants
+from epyt_flow.topology import flowunit_to_str
 
 
 if __name__ == "__main__":
@@ -40,15 +40,19 @@ if __name__ == "__main__":
         # Which flow units are used in this NetworkTopology instance?
         print(flowunit_to_str(topo.flow_units))
 
-        # Convert units to SI METRIC -- i.e. pipe diameter in *millimeter*,
-        # pipe length in *meter*, node elevation in *meter*, ...
-        new_topo = topo.convert_units(UNITS_SIMETRIC)
+        # Convert units to SI METRIC -- i.e. flow units to cubic meter per hours and pressure to meter.
+        # This will results yield pipe diameter in *millimeter*, pipe length in *meter*,
+        # node elevation in *meter*, ...
+        new_topo = topo.convert_units(flow_units=EpanetConstants.EN_CMH,
+                                      pressure_units=EpanetConstants.EN_METERS)
 
         print(new_topo.get_node_info("2"))
         print(new_topo.get_link_info("10"))
 
-        # Convert units back to US CUSTOMARY
-        new_topo = new_topo.convert_units(UNITS_USCUSTOM)
+        # Convert units back to US CUSTOMARY -- i.e., flow units should be gallons per minute
+        # and pressure in psi
+        new_topo = new_topo.convert_units(flow_units=EpanetConstants.EN_GPM,
+                                          pressure_units=EpanetConstants.EN_PSI)
 
         print(new_topo.get_node_info("2"))
         print(new_topo.get_link_info("10"))
